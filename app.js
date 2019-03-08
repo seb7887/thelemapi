39098474953636e0eb70dae896b0dba9e1fcc9ee
Sebastian <segurasebastian7887@gmail.com>
2019-03-08 13:54:39 -0300

    Morgan + Winston logger

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
+const logger = require('./logger');
 const routes = require('./routes');
+
+app.use(
+  morgan('combined', {
+    stream: {
+      write: message => {
+        logger.info(message);
+      },
+    },
+  }),
+);
 
 app.use('/', routes);
 
