813189b46543e044077a408c54ee6d55544f9300
Sebastian <segurasebastian7887@gmail.com>
2019-03-11 15:35:05 -0300

    Auth tests

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,13 @@ module.exports = {
   modulePaths: ['<rootDir>'],
   displayName: 'thelemapi',
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.js'],
-  collectCoverageFrom: ['!**/node_modules/**', '!**/__tests__/**'],
+  testMatch: ['**/__tests__/**/*.js', '**/test/**/*.js'],
+  collectCoverageFrom: [
+    '**/controllers/**',
+    '!**/data/**',
+    '!**/node_modules/**',
+    '!**/__tests__/**',
+  ],
   watchPlugins: [
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname',
