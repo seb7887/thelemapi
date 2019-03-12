bacc5888a614ab451275eb00f53494b92ed7cd9b
Sebastian <segurasebastian7887@gmail.com>
2019-03-12 15:57:19 -0300

    Brute Force Protection

--- error/index.js
+++ error/index.js
@@ -1,9 +1,13 @@
 const Sentry = require('@sentry/node');
 const logger = require('../logger');
 
-Sentry.init({
-  dsn: 'https://83c50456a02545ef993a13561a1ff318@sentry.io/1413863',
-});
+const dev = process.env.NODE_ENV !== 'production';
+
+if (!dev) {
+  Sentry.init({
+    dsn: 'https://83c50456a02545ef993a13561a1ff318@sentry.io/1413863',
+  });
+}
 
 const logError = err => {
   logger.error(err);
@@ -14,8 +18,10 @@ const reportError = err => {
 };
 
 const errorHandler = (error, req, res, next) => {
-  logError(error);
-  reportError(error);
+  if (!dev) {
+    logError(error);
+    reportError(error);
+  }
   return res.status(error.status || 500).json({
     error: {
       message: error.message || 'Something went wrong',
